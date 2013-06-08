9fb10d1ff56e0073f4b07151ed0e7072941dd72d
Nguyen Tuan Anh <tuananh00473@gmail.com>
2013-06-08 13:54:29 +0700

    red testShouldReturn0WithEmptyString

---
+++ StringCalculator0806/src/main/java/StringCalculator.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Admin
+ * Date: 6/8/13
+ * Time: 1:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class StringCalculator {
+    public static int add(String s) {
+        return -1;
+    }
+}
---
+++ StringCalculator0806/src/test/java/testStringCalculator.java
@@ -0,0 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Admin
+ * Date: 6/8/13
+ * Time: 1:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class testStringCalculator {
+    @Test
+    public void testShouldReturn0WithEmptyString(){
+        Assert.assertEquals(0, StringCalculator.add(""));
+    }
+}
