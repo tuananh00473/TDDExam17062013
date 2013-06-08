04bdf26d2557a5f1909a4f6eedfdd800f0b1a3d0
Nguyen Tuan Anh <tuananh00473@gmail.com>
2013-06-08 14:34:32 +0700

    green testShouldReturnSumWithManyLongerDelimiter

--- StringCalculator0806/src/test/java/testStringCalculator.java
+++ StringCalculator0806/src/test/java/testStringCalculator.java
@@ -70,4 +70,9 @@ public class testStringCalculator {
     public void testShouldReturnSumWithManyDelimiter(){
         Assert.assertEquals(6, StringCalculator.add("//[*][%]\n1*2%3"));
     }
+
+    @Test
+    public void testShouldReturnSumWithManyLongerDelimiter(){
+        Assert.assertEquals(6, StringCalculator.add("//[***][%%%]\n1***2%%%3"));
+    }
 }
